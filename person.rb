5bff4ce992147f0b7b5c247b4cd5cb2199ef8490
Kagheni <Kaghenimbale@gmail.com>
2023-05-22 11:26:45 +0200

    Add getter in person class

---
+++ person.rb
@@ -0,0 +1,17 @@
+class Person
+    def initialize(name="Unknown", age, parent_permission = true)
+        @id = Random.rand(1..1000)
+        @name = name
+        @age = age
+    end
+
+    def id
+        @id
+    end
+    def name
+        @name
+    end
+    def age
+        @age
+    end
+end
