50870298ce84a42ec3955c92dc6f7ea124a9c8ce
Kagheni <Kaghenimbale@gmail.com>
2023-05-24 08:36:20 +0200

    Fix linter issues

--- student.rb
+++ student.rb
@@ -1,7 +1,8 @@
 require './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -10,6 +11,7 @@ class Student < Person
   def play_hooky
     '¯(ツ)/¯'
   end
+
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
