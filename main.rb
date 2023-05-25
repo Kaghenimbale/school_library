79c832637f30d9b65107bdb77c2fff6b1ee7967c
Kagheni <Kaghenimbale@gmail.com>
2023-05-25 11:03:23 +0200

    Fix linters error

--- main.rb
+++ main.rb
@@ -1,45 +1,56 @@
-require "./app"
+require './app'
 
 class Main
-    attr_reader :app
+  attr_reader :app
 
-    def initialize(app)
-        @app = app
+  def initialize(app)
+    @app = app
+  end
+
+  def main
+    loop do
+      display_menu
+      menu = gets.chomp.to_i
+      menu_choice(menu)
+      break if menu == 7
     end
+  end
 
-    def main
-        loop do
-        puts "Please choose an option by enterin a number:"
+  def display_menu
+    puts 'Please choose an option by enterin a number:'
 
-        puts "1 - List all books"
-        puts "2 - List all people"
-        puts "3 - Create a person"
-        puts "4 - Create a book"
-        puts "5 - Create a rental"
-        puts "6 - List all the rental for a given person id"
-        puts "7 - Exit"
-    
-        menu = gets.chomp
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all the rental for a given person id'
+    puts '7 - Exit'
+  end
 
-        case menu.to_i
-        when 1
-            @app.list_book
-        when 2
-            @app.list_person
-        when 3
-            @app.add_person
-        when 4
-            @app.create_book
-        when 5
-            @app.create_rental
-        when 6
-            @app.list_rentals
-        when 7
-            puts "Thank you for using this app!"
-            exit
-        end
+  def menu_choice(menu)
+    menu_actions = {
+      1 => :list_book,
+      2 => :list_people,
+      3 => :add_person,
+      4 => :create_book,
+      5 => :create_rental,
+      6 => :list_rentals,
+      7 => :exit
+    }
+
+    action = menu_actions[menu]
+    if action
+      if action == :exit
+        puts 'Thank you for using this app!'
+        exit
+      else
+        @app.send(action)
+      end
+    else
+      puts 'Invalid choice. Please try again.'
     end
-    end
+  end
 end
 
 app = App.new
