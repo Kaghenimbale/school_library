cf978c7766cf77aa947d2a9870ff3869f2896cb4
danutibe07 <danielutibe07@gmail.com>
2023-05-29 14:07:45 +0100

    Modified main.rb

--- main.rb
+++ main.rb
@@ -1,48 +1,6 @@
 require './app'
-
-class DisplayMenu
-  def display_menu
-    puts 'Please choose an option by enterin a number:'
-
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all the rental for a given person id'
-    puts '7 - Exit'
-  end
-end
-
-class MenuChoice
-  def initialize(app)
-    @app = app
-  end
-
-  def menu_choice(menu)
-    menu_actions = {
-      1 => :list_book,
-      2 => :list_person,
-      3 => :add_person,
-      4 => :create_book,
-      5 => :create_rental,
-      6 => :list_rentals,
-      7 => :exit
-    }
-
-    action = menu_actions[menu]
-    if action
-      if action == :exit
-        puts 'Thank you for using this app!'
-        exit
-      else
-        @app.send(action)
-      end
-    else
-      puts 'Invalid choice. Please try again.'
-    end
-  end
-end
+require './menuchoice'
+require './displaymenu'
 
 class Main
   attr_reader :app
