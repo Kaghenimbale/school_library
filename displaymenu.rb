7eec2b0da3b6ea95660b0066ac22684ff944b377
danutibe07 <danielutibe07@gmail.com>
2023-05-29 14:09:05 +0100

    Added displaymenu.rb and menuchoice.rb files

---
+++ displaymenu.rb
@@ -0,0 +1,13 @@
+class DisplayMenu
+  def display_menu
+    puts 'Please choose an option by enterin a number:'
+
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all the rental for a given person id'
+    puts '7 - Exit'
+  end
+end
