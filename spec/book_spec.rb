90badf15f44bc87097ab59b37ff9ee1554b72f7d
danutibe07 <danielutibe07@gmail.com>
2023-05-31 12:23:46 +0100

    unit test for create_book

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../book'
+
+
+describe 'unit tests for book class' do
+  context 'have an instance of Book' do
+    book = Book.new('Talking Shoes', 'Peugeon Brothers')
+    it 'takes two parameters and returns a Book{"title","author"}' do
+      expect(book).to be_instance_of(Book)
+    end
+    it 'should have attributes' do
+      expect(book).to have_attributes(title: "Talking Shoes")
+      expect(book).to have_attributes(author: "Peugeon Brothers")
+    end
+  end
+
+end
