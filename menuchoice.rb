0c62acce30871b5d7e38ca8bda13ee94cedaf0c3
Kagheni <Kaghenimbale@gmail.com>
2023-05-31 07:04:51 +0200

    Fix the issue when exit

--- menuchoice.rb
+++ menuchoice.rb
@@ -1,3 +1,6 @@
+require './createjson'
+
+
 class MenuChoice
   MENU_ACTIONS = {
     1 => :list_book,
@@ -36,8 +39,8 @@ class MenuChoice
   end
 
   def create_json_files
-    CreateJsonBook.new(@app).create_file
-    CreateJsonPeople.new(@app).create_file
-    CreateJsonRental.new(@app).create_file
+    book = CreateJsonBook.new(@app).create_file
+    people = CreateJsonPeople.new(@app).create_file
+    rental = CreateJsonRental.new(@app).create_file
   end
 end
